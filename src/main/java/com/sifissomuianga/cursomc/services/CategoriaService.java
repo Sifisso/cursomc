a8c21762997fb056395d17fa238070db679ddc41
Sifisso <sifissom@gmail.com>
2021-01-10 13:48:47 +0200

    Ajuste no endpoint /categorias/{id} inexistente

--- src/main/java/com/sifissomuianga/cursomc/services/CategoriaService.java
+++ src/main/java/com/sifissomuianga/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.sifissomuianga.cursomc.domain.Categoria;
 import com.sifissomuianga.cursomc.repositories.CategoriaRepository;
+import com.sifissomuianga.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objecto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
